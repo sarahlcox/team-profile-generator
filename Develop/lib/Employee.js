4ce88c5e1a65deb5bcef9162d5c99d741e6272e3
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-21 17:19:50 -0500

    wrote engineer module

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,26 +1,32 @@
 // TODO: Write code to define and export the Employee class
 class Employee{
         constructor (name, id, email) {
-            this.name="sarah";
-            this.id=1;
-            this.email="sarah@test.com";
+            this.name=name;
+            this.id=id;
+            this.email=email;
         }
             getName(){
-        
+                return this.name;
             }
             getId(){
+                return this.id;
             
             }
     
             getEmail(){
+                return this.email;
             }
     
             getRole(){
+                return "Employee";
 
             }
 } 
 
-var ob = new Employee();
-console.log(ob);
+// var ob = new Employee("Tom",1, "t@test.com");
+// var ob2 = new Employee("Henry", 2, "h@test.com");
+
+// console.log(ob, ob2);
+// ob.getName();
 
 module.exports=Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee=require("./Employee");
+
+class Engineer extends Employee {
+    constructor (name, id, email, github) {
+        super(name,id,email);
+        this.github=github;
+    }
+    getRole(){
+        console.log("engineer");
+        // return "Engineer";
+    
+    }
+    getGithub() {
+        console.log(this.github)
+        // return this.github;
+    }
+}
+var ob = new Employee("Tom",1, "t@test.com", "username");
+var ob2 = new Employee("Henry", 2, "h@test.com","username");
+
+
+console.log(ob, ob2);
+ob.getName();
+module.exports=Engineer;
