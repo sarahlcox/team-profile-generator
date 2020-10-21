a1d406d5468d8379910489da1d5982c4803b7fa0
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-21 16:40:07 -0500

    started building constructor

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,11 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+        constructor (name, id, email) {
+            this.name=name;
+            console.log(name);
+            this.id=id;
+            console.log(id);
+            this.email=email;
+            console.log(email);
+        } 
+}
